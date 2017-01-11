9aa60991a4299763dd22491bd2459e49c83f2f39
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-11 13:44:58 +1100

    added auth wrapper and material tab ui

--- front-end/src/components/Auth/App/App.js
+++ front-end/src/components/Auth/App/App.js
@@ -12,12 +12,26 @@ import DebugTempLink from '../../../utils/DebugTempLink'
 
 //material-ui components
 import { AppBar,
-         TextField } from 'material-ui/';
+         TextField,
+         Tabs,
+         Tab } from 'material-ui/';
 
 //theme related material-ui
 import { MuiThemeProvider,
          getMuiTheme,
          darkBaseTheme } from 'material-ui/styles'
+
+//inline styles
+const styles = {
+    tabComponent : {
+        headline: {
+        fontSize: 24,
+        paddingTop: 16,
+        marginBottom: 12,
+        fontWeight: 400,
+    }
+  }
+}
 
 class App extends Component {
   constructor() {
@@ -43,6 +57,33 @@ class App extends Component {
             <DebugTempLink />
 
             <Header />
+
+            <Tabs
+        value={this.state.value}
+        onChange={this.handleChange}
+      >
+        <Tab label="Tab A" value="a" >
+          <div>
+            <h2 style={styles.headline}>Controllable Tab A</h2>
+            <p>
+              Tabs are also controllable if you want to programmatically pass them their values.
+              This allows for more functionality in Tabs such as not
+              having any Tab selected or assigning them different values.
+            </p>
+          </div>
+        </Tab>
+        <Tab label="Tab B" value="b">
+          <div>
+            <h2 style={styles.headline}>Controllable Tab B</h2>
+            <p>
+              This is another example of a controllable tab. Remember, if you
+              use controllable Tabs, you need to give all of your tabs values or else
+              you wont be able to select them.
+            </p>
+          </div>
+        </Tab>
+      </Tabs>
+
             <ReferralDashboard 
               _loadSampleReferral={this._loadSampleReferral}
               referrals={this.state.referrals}
