0a72a6730e0571076b757a51b3579f6121e25ff7
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-11 16:12:27 +1100

    updated referral sample data

--- front-end/src/components/Auth/App/_sample-referrals.js
+++ front-end/src/components/Auth/App/_sample-referrals.js
@@ -1,37 +1,55 @@
 module.exports = [
       {
       _id:            1,
-      name:           'steve jobs',
-      providerNumber: '11111'
+      name:           'Cardia, Brady',
+      providerNumber: '11111',
+      gender:         'M',
+      dateOfBirth:    '26/12/1950',
+      imgProfile:     'https://openclipart.org/image/2400px/svg_to_png/225296/Red-Heart.png'
       },
 
       {
       _id:            2,
-      name:           'steve wozzie',
-      providerNumber: '22222'
+      name:           'Pott, Jack',
+      providerNumber: '11111',
+      gender:         'M',
+      dateOfBirth:    '26/3/1967',
+      imgProfile:     'http://www.brainscat.com/wp-content/uploads/2015/09/jackpot.png'
       },
 
       {
       _id:            3,
-      name:           'sharon chen',
-      providerNumber: '333'
+      name:           'Flay, Sue',
+      providerNumber: '11111',
+      gender:         'F',
+      dateOfBirth:    '17/10/1954',
+      imgProfile:     'https://pbs.twimg.com/profile_images/2203940524/Souffle.jpg'
       },
 
       {
       _id:            4,
-      name:           'ian khor',
-      providerNumber: '4444'
+      name:           'Ball, Crystal',
+      providerNumber: '11111',
+      gender:         'F',
+      dateOfBirth:    '27/08/1959',
+      imgProfile:     'https://hydra-media.cursecdn.com/dota2.gamepedia.com/1/10/Crystal_Maiden.png?version=536fc77ea73fa5f2dc01a0036d199d52'
       },
 
       {
       _id:            5,
-      name:           'patrick smith',
-      providerNumber: '22222'
+      name:           'Lord, Star',
+      providerNumber: '11111',
+      gender:         'M',
+      dateOfBirth:    '3/1/1985',
+      imgProfile:     'http://static8.comicvine.com/uploads/square_medium/11117/111171699/3921775-starlord2014001_dc11-page-001.jpg'
       },
 
       {
       _id:            6,
-      name:           'neo smith',
-      providerNumber: '66'
+      name:           'Strange, Stephen',
+      providerNumber: '11111',
+      gender:         'F',
+      dateOfBirth:    '26/2/1966',
+      imgProfile:     'https://1.bp.blogspot.com/-qMiXPoEfddQ/V0VzoU0t8II/AAAAAAAADuE/jisTGhF6FikhSg-Rw9umzE0GHRc1iAM2QCLcB/s1600/doctor-strange-movie-all-we-need-to-know-about-film.png'
       }
 ]
