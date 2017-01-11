1fed85c1ee89821fd48a98ab35e2567b2f31d269
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-11 15:56:22 +1100

    added card to referral

--- front-end/src/components/Auth/App/EditableReferral.js
+++ front-end/src/components/Auth/App/EditableReferral.js
@@ -1,4 +1,13 @@
 import React, { Component } from 'react'
+import { Card, 
+         CardActions, 
+         CardHeader, 
+         CardMedia, 
+         CardTitle, 
+         CardText,
+         Toggle,
+         FlatButton } from 'material-ui'
+
 
 const style = {
     margin: '20px',
@@ -6,15 +15,67 @@ const style = {
 }
 
 class EditableReferral extends Component {
+
+    constructor(props) {
+        super(props)
+   
+        this.state = {
+            expanded: false,
+        }
+    }
+
+    handleExpandChange = (expanded) => {
+        this.setState({expanded: expanded})
+    }
+
+    handleToggle = (event, toggle) => {
+        this.setState({expanded: toggle})
+    }
+
+    handleExpand = () => {
+        this.setState({expanded: true})
+    }
+
+    handleReduce = () => {
+        this.setState({expanded: false})
+    }
+
     render(){
         return(
-            <div className="border" style={style}> 
-                <h1>This is a EditableReferral component</h1>
-                <h4>This DISPLAYS or ALLOWS EDITING a specific referral</h4>
-                <p>id : {this.props.referral._id}</p>
-                <p>name : {this.props.referral.name}</p>
-                <p>provider ID : {this.props.referral.providerNumber}</p>
-            </div>
+           <Card expanded={this.state.expanded} onExpandChange={this.handleExpandChange}>
+                <CardHeader
+                    title="URL Avatar"
+                    subtitle="Subtitle"
+                    avatar="http://static8.comicvine.com/uploads/square_medium/11117/111171699/3921775-starlord2014001_dc11-page-001.jpg"
+                    actAsExpander={true}
+                    showExpandableButton={true}
+                />
+                <CardText>
+                <Toggle
+                    toggled={this.state.expanded}
+                    onToggle={this.handleToggle}
+                    labelPosition="right"
+                    label="This toggle controls the expanded state of the component."
+                />
+                </CardText>
+                <CardMedia
+                expandable={true}
+                overlay={<CardTitle title="Overlay title" subtitle="Overlay subtitle" />}
+                >
+                    <img src="https://cdn.traileraddict.com/article/wp-content/uploads/2016/10/guardians-galaxy-wallpaper.jpg" />
+                </CardMedia>
+            <CardTitle title="Card title" subtitle="Card subtitle" expandable={true} />
+            <CardText expandable={true}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+            Donec mattis pretium massa. Aliquam erat volutpat. Nulla facilisi.
+            Donec vulputate interdum sollicitudin. Nunc lacinia auctor quam sed pellentesque.
+            Aliquam dui mauris, mattis quis lacus id, pellentesque lobortis odio.
+            </CardText>
+            <CardActions>
+            <FlatButton label="Expand" onTouchTap={this.handleExpand} />
+            <FlatButton label="Reduce" onTouchTap={this.handleReduce} />
+            </CardActions>
+      </Card>
         )
     }
 }
--- front-end/src/components/Auth/App/EditableReferralList.js
+++ front-end/src/components/Auth/App/EditableReferralList.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react'
 import EditableReferral from './EditableReferral'
 
 const style = {
-    margin: '20px',
+    margin: '5px',
     backgroundColor: 'gray'
 }
 
@@ -14,11 +14,6 @@ class EditableReferralList extends Component {
         console.log(this.props.referrals)
         return(
             <div className="border" style={style}> 
-                <h1>This is a EditableReferralList component</h1>
-                <h4>This DISPLAYS the list of referrals</h4>
-                <h4>There should be an array of referrals, use map to display</h4>
-
-                {/* code below is breaking, commented off for now*/}
                 {this.props.referrals.map( (jsonObject) => 
                     <EditableReferral referral={jsonObject} />
                 )}
