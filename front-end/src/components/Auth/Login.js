5f48520d54e66684ab979376e6cb76593a4611e3
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-12 23:43:04 +1100

    partial working authenticatio with auth0

--- front-end/src/components/Auth/Login.js
+++ front-end/src/components/Auth/Login.js
@@ -1,19 +1,48 @@
+// export default Login
+
 import React, { Component } from 'react'
 
 import DebugTempLink from '../../utils/DebugTempLink'
+import AuthService from './../../utils/AuthService'
+import RaisedButton from 'material-ui/RaisedButton'
+
+//Material UI theme
+import { MuiThemeProvider,
+         getMuiTheme,
+         darkBaseTheme } from 'material-ui/styles'
+
+import medrefrTheme from './../styles/Theme'
+
+const style = {
+  margin: 12,
+}
 
 class Login extends Component {
+    constructor(){
+        super()
+        this.authenticate = this.authenticate.bind(this)
+    }
+
+  authenticate(){
+    const auth = new AuthService('LdW1iARd0K080BlzpEI0vh6eitHuUceL',
+                                 'iankhor.au.auth0.com')
+    auth.login()
+  }
+
+
     render(){
         return(
+            <MuiThemeProvider muiTheme={medrefrTheme}>
             <div className="generic-center"> 
                 {/* temp onscreen redirection */}
                 <DebugTempLink />
                 <h1>This is a Login page</h1>
-                <h2>... Enter email and password ...</h2>
+                <RaisedButton label="Login" primary={true} style={style} onClick={this.authenticate}/>
             </div>
+            </MuiThemeProvider>
+
         )
     }
 }
 
 export default Login
-
---
+++ front-end/src/utils/AuthService.js
@@ -0,0 +1,50 @@
+import Auth0Lock from 'auth0-lock'
+import { browserHistory } from 'react-router'
+
+export default class AuthService {
+  constructor(clientId, domain) {
+    // Configure Auth0
+    this.lock = new Auth0Lock(clientId, domain, {
+      auth: {
+        redirectUrl: 'http://localhost:9000/Login',
+        responseType: 'token'
+      }
+    })
+    // Add callback for lock `authenticated` event
+    this.lock.on('authenticated', this._doAuthentication.bind(this))
+    // binds login functions to keep this context
+    this.login = this.login.bind(this)
+  }
+
+  _doAuthentication(authResult) {
+    // Saves the user token
+    this.setToken(authResult.idToken)
+    // navigate to the home route
+    browserHistory.replace('/')
+  }
+
+  login() {
+    // Call the show method to display the widget.
+    this.lock.show()
+  }
+
+  loggedIn() {
+    // Checks if there is a saved token and it's still valid
+    return !!this.getToken()
+  }
+
+  setToken(idToken) {
+    // Saves user token to local storage
+    localStorage.setItem('id_token', idToken)
+  }
+
+  getToken() {
+    // Retrieves the user token from local storage
+    return localStorage.getItem('id_token')
+  }
+
+  logout() {
+    // Clear user token and profile data from local storage
+    localStorage.removeItem('id_token');
+  }
+}
