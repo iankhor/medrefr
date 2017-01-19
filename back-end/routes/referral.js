f57aa2bdfb385cdfde10f5a151e0b70577a5da2f
iankhor <iankhorbc.thecoder@gmail.com>
2017-01-19 14:11:36 +1100

    updated referral GET request with error handling

--- back-end/routes/referral.js
+++ back-end/routes/referral.js
@@ -7,15 +7,27 @@ const router = express.Router();
 
 // Index: read all
 router.get('/', function(req, res, next) {
-    res.json({
-        message: "get all"
+    Referral.find()
+    .then( referrals => {
+        // res.json(referrals)
+        console.log(referrals.length)
+        referrals.length ? res.json({ message: "No content"}) : res.status(204).json({ message: "No content"})
     })
-});
+    .catch( () => {
+        res.status(500).json({
+            message: "Error : Internal server error"
+        })
+    }) 
+})
 
 // Index: read all
 router.post('/', function(req, res, next) {
     res.json(sampleReferral.referral1)
 
-});
+    //     Counter.create({
+    //     count: 0
+    // })
+
+})
 
 module.exports = router;
