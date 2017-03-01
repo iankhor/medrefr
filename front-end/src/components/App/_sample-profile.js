3d2cb2660055c01bdd8a8ca5e710d591e9709c98
iankhor <iankhorbc.thecoder@gmail.com>
2017-03-01 15:35:06 +1100

    updated sample profile

--- front-end/src/components/App/_sample-profile.js
+++ front-end/src/components/App/_sample-profile.js
@@ -1,13 +1,40 @@
 module.exports = {
       triage: {
-            "role":                       "triage",
+            "email"             :  "triage@triage.com",
+            "surname"           :  "Greg",
+            "giveName"          :  "Sam",
+            "dateOfBirthDay"    :  "26",
+            "dateOfBirthMonth"  :  "9",
+            "dateOfBirthYear"   :  "1983",
+            "providerNumber"    :  "123478",
+            "contactNumber"     :  "0000000",
+            "primaryPlaceOfPractice" :  "Somewhere",
+            "role"              :  "triage"
       },
 
       gp: {
-            "role":                       "gp",
+            "email"             :  "triage@triage.com",
+            "surname"           :  "Khor",
+            "giveName"          :  "Ian",
+            "dateOfBirthDay"    :  "26",
+            "dateOfBirthMonth"  :  "9",
+            "dateOfBirthYear"   :  "1983",
+            "providerNumber"    :  "123478",
+            "contactNumber"     :  "0000000",
+            "primaryPlaceOfPractice" :  "Somewhere",
+            "role"              : "gp",
       },
 
       psychiatrist: {
-            "role":                       "psychiatrist",     
+            "email"             :  "triage@triage.com",
+            "surname"           :  "Chen",
+            "giveName"          :  "Sharon",
+            "dateOfBirthDay"    :  "26",
+            "dateOfBirthMonth"  :  "9",
+            "dateOfBirthYear"   :  "1983",
+            "providerNumber"    :  "123478",
+            "contactNumber"     :  "0000000",
+            "primaryPlaceOfPractice" :  "Somewhere",
+            "role"              : "psychiatrist",     
       }
 }
